809602f5c6b1696c5e96b8593faac558a3c66d9b
Ethan Stoneman <etst9006@colorado.edu>
2024-05-03 11:30:44 -0600

    move `print_header` to proper location

--- src/dfs_util.c
+++ src/dfs_util.c
@@ -41,7 +41,8 @@ char *realloc_buf(char *buf, size_t size) {
 size_t strip_hdr(char *buf, DFCHeader *dfc_hdr) {
   size_t len_hdr;
 
-  strncpy(dfc_hdr->cmd, buf, strlen(buf) + 1);
+  // strncpy(dfc_hdr->cmd, buf, strlen(buf) + 1);
+  strncpy(dfc_hdr->cmd, buf, sizeof(dfc_hdr->cmd) + 1);
   len_hdr = strlen(buf) + 1;
 
   strncpy(dfc_hdr->fname, buf + len_hdr, strlen(buf + len_hdr) + 1);
@@ -49,6 +50,8 @@ size_t strip_hdr(char *buf, DFCHeader *dfc_hdr) {
 
   memcpy(&dfc_hdr->offset, buf + len_hdr, sizeof(size_t));
   len_hdr += sizeof(size_t);
+
+  print_header(dfc_hdr);
 
   return len_hdr;
 }
@@ -69,4 +72,12 @@ size_t strnins(char *dst, const char *src, size_t n) {
   strncpy(dst + src_len - 1, tmp, dst_len);
 
   return n;
+}
+
+void print_header(DFCHeader *dfc_hdr) {
+  fputs("DFCHeader {\n", stderr);
+  fprintf(stderr, "  cmd: %s\n", dfc_hdr->cmd);
+  fprintf(stderr, "  filename: %s\n", dfc_hdr->fname);
+  fprintf(stderr, "  offset: %zu\n", dfc_hdr->offset);
+  fputs("}\n", stderr);
 }
